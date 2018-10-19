819e2e58fbcb18f2f935d5bd788ae3c488f6551f
speedy-eggbert <frushyfan@gmail.com>
2018-10-18 20:15:28 -0500

    Initial commit

---
+++ src/main/groovy/codes/chelsea/okeinote/okeinoteservice/OkeinoteServiceApplication.groovy
@@ -0,0 +1,12 @@
+package codes.chelsea.okeinote.okeinoteservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class OkeinoteServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run OkeinoteServiceApplication, args
+	}
+}
---
+++ src/test/groovy/codes/chelsea/okeinote/okeinoteservice/OkeinoteServiceApplicationTests.groovy
@@ -0,0 +1,16 @@
+package codes.chelsea.okeinote.okeinoteservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class OkeinoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
